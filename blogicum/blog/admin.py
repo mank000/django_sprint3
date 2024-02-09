ccdaec80f6886a913a2e61bcc056ce093f6e4a6d
Козьмин Артём <kozmin_arti@mail.ru>
2024-02-09 12:11:50 +0700

    update

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,5 +1,6 @@
-from django.contrib import admin
 from .models import Category, Location, Post
+from django.contrib import admin
+
 
 admin.site.register(Category)
 admin.site.register(Location)
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,29 +1,49 @@
-import datetime
+import datetime as dt
+from blog.models import Post, Category
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
-from blog.models import Post, Category
 
-TIME_NOW = datetime.datetime.now()
+NUM_POSTS = 5
+
+
+def get_post_list():
+    post_list = Post.objects.all().filter(is_published=True,
+                                          pub_date__lte=dt.datetime.now(),
+                                          category__is_published=True)
+    return post_list
+
+
+def get_post_detail(post_id: int):
+    post_detail = get_object_or_404(Post,
+                                    Q(pk=post_id)
+                                    & Q(pub_date__lte=dt.datetime.now())
+                                    & (Q(is_published=True)
+                                        & Q(category__is_published=True)))
+    return post_detail
+
+
+def get_categotry_posts(category_slug: str):
+    category = get_object_or_404(Category,
+                                 Q(slug=category_slug)
+                                 & Q(is_published=True))
+    post_list = Post.objects.filter(category_id=category.id,
+                                    is_published=True,
+                                    pub_date__lte=dt.datetime.now())
+    return [category, post_list]
 
 
 def index(request):
     template = 'blog/index.html'
-    post_list = Post.objects.all().filter(is_published=True,
-                                          pub_date__lte=TIME_NOW,
-                                          category__is_published=True)
+    post_list = get_post_list()
     context = {
-        'post_list': post_list[:5],
+        'post_list': post_list[:NUM_POSTS],
     }
     return render(request, template, context)
 
 
 def post_detail(request, post_id: int):
     template = 'blog/detail.html'
-    post = get_object_or_404(Post,
-                             Q(pk=post_id)
-                             & Q(pub_date__lte=TIME_NOW)
-                             & (Q(is_published=True)
-                                & Q(category__is_published=True)))
+    post = get_post_detail(post_id)
     context = {
         'post': post,
     }
@@ -32,14 +52,9 @@ def post_detail(request, post_id: int):
 
 def category_posts(request, category_slug: str):
     template = 'blog/category.html'
-    category = get_object_or_404(Category,
-                                 Q(slug=category_slug)
-                                 & Q(is_published=True))
-    post_list = Post.objects.filter(category_id=category.id,
-                                    is_published=True,
-                                    pub_date__lte=TIME_NOW)
+    category_post_list = get_categotry_posts(category_slug)
     context = {
-        'category': category,
-        'post_list': post_list,
+        'category': category_post_list[0],
+        'post_list': category_post_list[1],
     }
     return render(request, template, context)
